8cada26132d47d601d7c4a32de8f7031b67dd224
Walking27 <76839270+Walking27@users.noreply.github.com>
2021-02-24 15:17:27 +0300

    23 Şubat

---
+++ Business/DependevcyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrate;
+using DataAccess.Abstract;
+using DataAccess.Concrate.EntityFramwork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependevcyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+        }
+    }
+}
