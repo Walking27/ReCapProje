6127fd7eea7d39c4e07270ba0e03795d46c843c3
Walking27 <76839270+Walking27@users.noreply.github.com>
2021-02-25 13:12:51 +0300

    24 Şubat Gün Sonu

--- Business/DependevcyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependevcyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrate;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrate.EntityFramwork;
 using System;
@@ -15,6 +18,16 @@ namespace Business.DependevcyResolvers.Autofac
         {
             builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
             builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+
         }
+
+
     }
 }
